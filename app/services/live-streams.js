d4558377b8c37fd04b7aa6cc8dfb166c42e7efcf
Houndsto0th <bry.kinnear@gmail.com>
2016-07-21 14:23:16 -0600

    Fixed twitch service

--- app/services/live-streams.js
+++ app/services/live-streams.js
@@ -2,14 +2,21 @@ import Ember from 'ember';
 
 export default Ember.Service.extend({
 
-  getOnlineStreamers(){
+  async getOnlineStreamers(){
     let url = 'https://api.twitch.tv/kraken/streams/';
-    let guildStreamers = ['tanned_priest', 'hounds_bdk', 'legendarylea'];
+    let guildStreamers = ['tanned_priest', 'houndsto0th_', 'legendarylea'];
     let onlineStreamers = guildStreamers.map((streamSuffix) => {
-      let channel = Ember.$.get( url + streamSuffix );
-      console.log(channel);
+      let thisStream = null;
+      let channel = Ember.$.get( url + streamSuffix ).then((response) => {
+        thisStream = response.stream;
+      });
+      if( thisStream !== null ) {
+        return thisStream
+      } else {
+        return null;
+      }
     });
-
+    return onlineStreamers
   }
 
 });
