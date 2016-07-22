4a9cb943047503c6ac9c37801149ed7e97a78470
JoshuaPoulin <jpoulin87@gmail.com>
2016-07-22 10:08:25 -0600

    updated with vindi streamers

--- app/homepage/route.js
+++ app/homepage/route.js
@@ -6,16 +6,18 @@ export default Ember.Route.extend({
   liveStreams: Ember.inject.service(),
 
  async model() {
-    let guildStreamers = [{streamKey:'tanned_priest', classId: 5}, 
-                          {streamKey:'houndsto0th_'}, 
-                          {streamKey:'legendarylea'}, 
-                          {streamKey:'slootbag', classId: 1}];
+    let guildStreamers = [{streamKey:'tanned_priest', classId: 5},
+                          {streamKey:'houndsto0th_' classId: 6},
+                          {streamKey:'facial_sh0tzz', classID: 3},
+                          {streamKey:'draakken', classId: 11},
+                          {streamKey:'cedx10', classId: 5},
+                          {streamKey:'mypantaloonz', classId: 12}];
     let activeStreams = [];
 
     for (let streamer of guildStreamers) {
       let channel = await this.get('liveStreams').getOnlineStreamers(streamer.streamKey).then((response) => {
         response.class = streamer.classId;
-        
+
         if (response.stream !== null) {
           activeStreams.push(response);
 
@@ -29,13 +31,13 @@ export default Ember.Route.extend({
       let members = allMembers.filter( member => member.rank < 6);
       let raidersNoOfficerAltsIncluded = members.filter( member => member.rank !== 2);
       let raidersNoAltsIncluded = raidersNoOfficerAltsIncluded.filter( member => member.rank !== 4);
-      
+
       return { raidersNoAltsIncluded };
     });
-    
-    
+
+
     return { memberData, activeStreams };
-    
-    
+
+
   }
 });
