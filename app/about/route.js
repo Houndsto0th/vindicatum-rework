1c79e1b2f199c1d11aaf4ca6c71bf991ebd2bde5
JoshuaPoulin <jpoulin87@gmail.com>
2016-07-23 17:48:56 -0600

    added husk of about page

---
+++ app/about/route.js
@@ -0,0 +1,23 @@
+import Ember from 'ember';
+
+export default Ember.Route.extend({
+  vindicatumMembers: Ember.inject.service(),
+
+  async model() {
+
+     let memberData = await this.get('vindicatumMembers').getVindiMembers().then((response) => {
+       let allMembers = response.members;
+       let members = allMembers.filter( member => member.rank < 6);
+       let raidersNoOfficerAltsIncluded = members.filter( member => member.rank !== 2);
+       let raidersNoAltsIncluded = raidersNoOfficerAltsIncluded.filter( member => member.rank !== 4);
+
+       return { raidersNoAltsIncluded };
+     });
+
+
+     return { memberData };
+
+
+   }
+
+});
--- app/router.js
+++ app/router.js
@@ -8,6 +8,7 @@ var Router = Ember.Router.extend({
 
 Router.map(function() {
   this.route('homepage');
+  this.route('about');
 });
 
 export default Router;
