752f9aa6371d16e76b3c724aa025c51902bcc0a4
Houndsto0th <bry.kinnear@gmail.com>
2016-08-14 17:37:39 -0600

    Further build changes

--- ember-cli-build.js
+++ ember-cli-build.js
@@ -13,8 +13,8 @@ module.exports = function(defaults) {
     }
   });
 
-  app.import(app.bowerDirectory + '/bootstrap/dist/js/bootstrap.js');
-  app.import(app.bowerDirectory + '/bootstrap/dist/css/bootstrap.css');
+  // app.import(app.bowerDirectory + '/bootstrap/dist/js/bootstrap.js');
+  // app.import(app.bowerDirectory + '/bootstrap/dist/css/bootstrap.css');
 
   // Use `app.import` to add additional libraries to the generated
   // output files.
