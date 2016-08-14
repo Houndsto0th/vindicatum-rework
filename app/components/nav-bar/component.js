84d682802e470d71248fd5f3d8322b8c057c48db
Houndsto0th <bry.kinnear@gmail.com>
2016-08-14 17:51:25 -0600

    Fully removed side-menu

--- app/components/nav-bar/component.js
+++ app/components/nav-bar/component.js
@@ -4,12 +4,6 @@ export default Ember.Component.extend({
   sideMenu: Ember.inject.service(),
 
   actions: {
-    openSideMenu() {
-      if (this.get('sideMenu').progress == 100) {
-        this.get('sideMenu').close();
-      } else {
-        this.get("sideMenu").open();
-      }
-    }
+    
   }
 });
