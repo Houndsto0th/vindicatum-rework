b0a61ea3726d57c4af2efadfb162c06995669450
JoshuaPoulin <jpoulin87@gmail.com>
2016-09-08 13:46:22 -0600

    bandaid fix for imgur auth

--- app/services/homepage-blog.js
+++ app/services/homepage-blog.js
@@ -3,11 +3,11 @@ import Ember from 'ember';
 export default Ember.Service.extend({
 
   getVindiAlbum(){
-  
+
     let userAlbums = Ember.$.ajax({
       url: 'https://api.imgur.com/3/album/y0TxV',
       headers: {
-          'Authorization':'Bearer 98ea9688a6142d8486dcd39f892efc5c229c9a38'
+          'Authorization':'Bearer 3d4c945f23cf7dd9a626799e6db6b0acbeb3f2e7'
       },
       method: 'GET'
       });
