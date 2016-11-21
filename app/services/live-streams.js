2f0814edb8f22179264f881032daf073361193c2
JoshuaPoulin <jpoulin87@gmail.com>
2016-11-20 20:26:27 -0700

    uninished twitch api

--- app/services/live-streams.js
+++ app/services/live-streams.js
@@ -9,4 +9,15 @@ export default Ember.Service.extend({
 
     return channel
   }
+  getOnlineStreamers(streamSuffix){
+    let endPoint = 'https://api.twitch.tv/kraken/streams/';
+    let channel = Ember.$.ajax({
+      url: endPoint + streamSuffix,
+      headers: {
+        'Client-ID': (string goes here)
+      },
+      method:: 'GET'
+    })
+    return channel;
+  }
 });
