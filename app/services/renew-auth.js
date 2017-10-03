552cf0ade396749bd1c6c844391fe433435782de
Houndsto0th <bry.kinnear@gmail.com>
2017-10-03 14:22:34 -0600

    Updated ajax request

--- app/services/renew-auth.js
+++ app/services/renew-auth.js
@@ -9,7 +9,8 @@ export default Ember.Service.extend({
         'client_id': '077e7c5fb98ccd9',
         'client_secret': '0421bab8d04a0b3fdd3817865942ec5ab4e2e05c',
         'refresh_token': '5aa0751f6059fc22a0a04fde304810fe562faced',
-        'grant_type': 'refresh_token'
+        'grant_type': 'refresh_token',
+        'Access-Control-Allow-Origin': 'http://vindicatum.com'
       },
       method: 'POST'
     });
